deec3becfda69f3beaf81a182b6584a1813e61e7
leminionn <minhlephan1901@gmail.com>
2026-03-17 21:32:59 +0700

    feat(auth): implement user login functionality with JWT authentication

---
+++ taskpilot-infrastructure/src/main/java/com/taskpilot/infrastructure/config/security/JwtService.java
@@ -0,0 +1,34 @@
+package com.taskpilot.infrastructure.config.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = secretKey.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey())
+                .compact();
+    }
+}
---
+++ taskpilot-users/src/main/java/com/taskpilot/users/auth/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.taskpilot.users.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank(message = "Email is required") String email,
+        @NotBlank(message = "Password is required") String password
+) {
+}
