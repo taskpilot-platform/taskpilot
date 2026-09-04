1ec94ac3e645eda1ad6e656ea067f6a51361bbe9
phuthien2005 <dangphuthien2005@gmail.com>
2026-09-04 13:30:42 +0000

    refactor(wave-1.3): streamline SprintService using ProjectSecurityService and ValidationUtils (-42 LOC)

--- taskpilot-users/src/main/java/com/taskpilot/users/admin/controller/AdminSettingsController.java
+++ taskpilot-users/src/main/java/com/taskpilot/users/admin/controller/AdminSettingsController.java
@@ -8,8 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- taskpilot-users/src/main/java/com/taskpilot/users/skills/controller/SystemSkillController.java
+++ taskpilot-users/src/main/java/com/taskpilot/users/skills/controller/SystemSkillController.java
@@ -6,7 +6,6 @@ import com.taskpilot.users.skills.service.SkillService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
